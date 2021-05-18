15e03f1a2352ef4567f936da0d4d07e8d5137630
DerpHead <alexraia4@gmail.com>
2021-05-18 07:29:14 -0400

    derp

--- server/index.js
+++ server/index.js
@@ -1,13 +1,29 @@
 require('dotenv').config();
+const massive = require('massive');
 const express = require('express'),
       userCtrl = require('./controllers/user'),
       postCtrl = require('./controllers/posts')
 
 
+
+
+
 const app = express();
 
-app.use(express.json());
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
+
+massive({
+    connectionString: CONNECTION_STRING,
+    ssl: {rejectUnauthorized: false}
+}).then(dbInstance => {
+    app.set("db", dbInstance);
+    console.log("database connected");
+}).catch(err => console.log(err));
+
+
+app.use(express.json());
+/*
 //Auth Endpoints
 app.post('/api/auth/register', userCtrl.register);
 app.post('/api/auth/login', userCtrl.login);
@@ -19,5 +35,7 @@ app.get('/api/posts', postCtrl.readPosts);
 app.post('/api/post', postCtrl.createPost);
 app.get('/api/post/:id', postCtrl.readPost);
 app.delete('/api/post/:id', postCtrl.deletePost)
-
-app.listen(4000, _ => console.log(`running on ${4000}`));
+*/
+app.listen(SERVER_PORT, () => {
+    console.log(`Server listening on port ${SERVER_PORT}.`);
+  });
